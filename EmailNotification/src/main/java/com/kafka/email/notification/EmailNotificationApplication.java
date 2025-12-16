33931a46496a507c9b2a9d6842a5537b3cfca6df
Roshan <sahuroshan96@gmail.com>
2025-12-16 19:45:41 +0530

    added error handling for consumer

--- EmailNotification/src/main/java/com/kafka/email/notification/EmailNotificationApplication.java
+++ EmailNotification/src/main/java/com/kafka/email/notification/EmailNotificationApplication.java
@@ -2,6 +2,8 @@ package com.kafka.email.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmailNotificationApplication {
@@ -9,5 +11,10 @@ public class EmailNotificationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmailNotificationApplication.class, args);
 	}
+
+    @Bean
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
--- EmailNotification/src/main/java/com/kafka/email/notification/KafkaConsumerConfiguration.java
+++ EmailNotification/src/main/java/com/kafka/email/notification/KafkaConsumerConfiguration.java
@@ -1,16 +1,23 @@
 package com.kafka.email.notification;
 
+import com.kafka.email.notification.error.NotRetryableException;
+import com.kafka.email.notification.error.RetryableException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +28,17 @@ public class KafkaConsumerConfiguration {
     @Autowired
     Environment environment;
 
+    /**
+     * Creates and configures a Kafka ConsumerFactory.
+     *
+     * This method defines how Kafka consumers should be created:
+     * - Where Kafka is running (bootstrap servers)
+     * - How message keys and values are deserialized
+     * - Which consumer group the consumer belongs to
+     * - How deserialization errors are handled safely
+     *
+     * This factory is later used by Kafka listeners to create consumers.
+     */
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
@@ -33,10 +51,39 @@ public class KafkaConsumerConfiguration {
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
+    /**
+     * Creates the KafkaListenerContainerFactory used by @KafkaListener.
+     *
+     * This method defines how Kafka listener containers should run
+     * Spring uses this factory automatically when it finds @KafkaListener methods.
+     */
     @Bean
-    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactory) {
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory,
+            KafkaTemplate<String, Object> kafkaTemplate) {
+
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
+                new DeadLetterPublishingRecoverer(kafkaTemplate), new FixedBackOff(5000, 3));
+        errorHandler.addNotRetryableExceptions(NotRetryableException.class);
+        errorHandler.addRetryableExceptions(RetryableException.class);
+
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler);;
         return factory;
+    }
+
+    @Bean
+    KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
     }
 }
